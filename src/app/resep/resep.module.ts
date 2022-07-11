c93140389d07c620590dbfb26d72a668fbc20569
Edo Bagus <edobagusp14@gmail.com>
2022-07-11 16:56:37 +0700

    routing dengan id

--- src/app/resep/resep.module.ts
+++ src/app/resep/resep.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ResepPageRoutingModule } from './resep-routing.module';
 
 import { ResepPage } from './resep.page';
+import { DetailComponent } from './detail/detail.component';
+const routes: Routes = [
+{
+path:'',
+component: ResepPage
+},
+{
+  path:':id',
+  component: DetailComponent
+  }
+
+]
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ResepPageRoutingModule
+    ResepPageRoutingModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ResepPage]
+  declarations: [ResepPage, DetailComponent]
 })
 export class ResepPageModule {}
